1dc09bc447fd4ebcf4c19b28853c81932d5b5604
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2021-07-29 14:12:52 -0300

    Aula Webhooks do Stripe

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const Webhooks = (req: NextApiRequest, res: NextApiResponse) => {
+    console.log('evento recebido');
+
+    res.status(200).json({
+        ok: true
+    })
+}
+
+export default Webhooks;
