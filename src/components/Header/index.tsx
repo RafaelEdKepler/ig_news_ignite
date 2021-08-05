715032c76e93ae2d04366d8b889dc1e046917d9e
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2021-08-05 15:34:05 -0300

    Aula Componente: ActiveLink - Finalizada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,23 @@
 import { SignInButton } from '../SigninButton';
 import Link from 'next/link';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+    const { asPath } = useRouter();
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news"/>
                 <nav>
-                    <Link href="/">
-                        <a className={styles.active} >Home</a>
-                    </Link>
-                    <Link href="/posts" prefetch>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
                         <a>Posts</a>
-                    </Link>
+                    </ActiveLink>
                 </nav>
                 <SignInButton/>
             </div>
