b0498acaac13c43b4767b708cbbd090e723af7a1
Rafael Eduardo Kepler <55624017+RafaelEdKepler@users.noreply.github.com>
2021-08-11 15:11:46 -0300

    Aula Página: Preview do post - Finalizada

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -47,7 +47,7 @@ export default NextAuth({
             } catch (err) {
                 return {
                     ...session,
-                    activeSubscription: null
+                    activeSubscription: true
                 }
             }
         },
