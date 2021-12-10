f23a6c98aca7ae4775f1dddb0497d06c89ce07b3
Rafael Eduardo Kepler <rafael.kepler@compasso.com.br>
2021-12-10 10:09:35 -0300

    Aula Dicas de Testes assincronas - Finalizada

---
+++ src/components/Async/Async.spec.tsx
@@ -0,0 +1,18 @@
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import { Async } from '.';
+
+test('it renders correctly', async () => {
+    render(<Async />);
+
+    expect(screen.getByText('Hello World')).toBeInTheDocument();
+    //expect(await screen.findByText('Button')).toBeInTheDocument(); // Espera algo aparecer em tela, como timeout maximo de 1 segundo
+
+    await waitForElementToBeRemoved(screen.queryByText('Button')); // Verifica se o elemento nao esta na tela
+
+    await waitFor(() => {
+        return expect(screen.getByText('Hello World')).toBeInTheDocument();
+        // return expect(screen.queryByText('Hello World')).not.toBeInTheDocument(); // Verifica se um elemento nao esta na tela
+    }, {
+        timeout: 4000
+    }) // Posso definir o timeout que eu achar necessario, se nao definir vai assumir 1 segundo tambem.
+})
